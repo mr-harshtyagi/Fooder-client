a95c9c45850ca30255797d618a18e8b924f0b3fa
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-04-08 10:57:53 +0530

    update 08-04 10:57

--- src/pages/restaurent-view.jsx
+++ src/pages/restaurent-view.jsx
@@ -1,28 +1,39 @@
 import Navbar from "../components/navbar"
 import Appbar from "../components/appbar";
-import { useParams } from "react-router-dom";
- import { useState,useEffect } from "react";
+import { useParams,useNavigate } from "react-router-dom";
+import { useState,useEffect } from "react";
 import Dish from "../components/dish";
+import axios from "axios";
 
 export default function RestaurentView() {
-  const [restaurents, setRestaurents] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:5000/restaurents")
-      .then((res) => res.json())
-      .then((json) => setRestaurents(json));
-  }, []);
   let params = useParams();
-  
+  let navigate =useNavigate();
+  const [restaurent, setRestaurent] = useState({});
+  const [isLoaded, setIsLoaded] =useState(false);
+  useEffect(() => {
+        axios
+          .get(
+            `http://localhost:5000/getrestaurentdetails/${params.restaurentId}`
+          )
+          .then(function (response) {
+            setRestaurent(response.data)
+            setIsLoaded(true)
+            if(response.data.status === false)
+            navigate("/")
+            
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+
+  }, []);
   
   
   return (
     <div>
       <Navbar />
-      {restaurents
-        .filter((restaurent) => restaurent.id === Number(params.restaurentId))
-        .map((filteredRestaurent) => {
-          return (
-            <div
+      {isLoaded ?(
+         <div
               className="restaurent-view"
               style={{
                 padding: "20px",
@@ -40,7 +51,7 @@ export default function RestaurentView() {
                   boxShadow: "0px 0px 40px -20px rgba(87,84,87,1)",
                 }}
               >
-                {" " + filteredRestaurent.rating}
+                {" " + restaurent.rating}
                 <hr style={{ border: "none" }} />
                 <>Rating</>
               </h6>
@@ -49,18 +60,17 @@ export default function RestaurentView() {
                   fontSize: "2rem",
                 }}
               >
-                {filteredRestaurent.name}
+                {restaurent.name}
               </h5>
               <h6 style={{ fontSize: "0.8rem", color: "grey" }}>
-                {filteredRestaurent.type} | {filteredRestaurent.category}
+                {restaurent.type} | {restaurent.category}
               </h6>
 
               <div>
                 <h4 style={{ marginTop: "40px" }}>
                   <strong>Menu (5)</strong>{" "}
                 </h4>
-                {
-          filteredRestaurent.dish.map((dish) =>
+                {restaurent.dish.map((dish) =>
               dish.status && (
                 <Dish
                   key={dish.id}
@@ -75,7 +85,11 @@ export default function RestaurentView() {
           )}
               </div>
             </div>
-          );})}
+
+
+      ): ( <h1>Loading ...</h1>)    }
+           
+         
       <Appbar />
     </div>
   );
