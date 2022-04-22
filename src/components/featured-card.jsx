a64550d91f3ba6552889bfec2a77fa7e1599ff9b
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-04-22 21:52:04 +0530

    add imagur link

--- src/components/featured-card.jsx
+++ src/components/featured-card.jsx
@@ -1,13 +1,13 @@
 
 export default function Card() {
   return (
-    <div style={{ marginBottom:"20px" }} className="slider-card">
+    <div style={{ marginBottom: "20px" }} className="slider-card">
       {/* <h1>This is the slider/card component</h1>
       <h1>This is the slider/card component</h1>
       <h1>This is the slider/card component</h1> */}
       <img
-        style={{ borderRadius: "20px", height: "250px"}}
-        src="images/food.png"
+        style={{ borderRadius: "20px", height: "250px" }}
+        src="https://i.imgur.com/WZa1kdb.png"
         alt="food-img"
       />
     </div>
