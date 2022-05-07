83a471137a33f5d44c3d23a7d05eede3d336eaf2
yshv-2022 <yashasvichaudhary14@gmail.com>
2022-05-07 09:52:58 +0530

    order field added

--- src/components/order.jsx
+++ src/components/order.jsx
@@ -1,1 +1,36 @@
 // for making this order component , use dish for knowledge and reference
+
+export default function Order(props) {
+  
+  return (
+    <div
+      style={{
+        padding: "10px",
+        textAlign: "left",
+        width: "100%",
+        backgroundColor: "#F9F3EE",
+        borderRadius: "10px",
+        marginBottom:"20px"
+      }}
+    >
+      <h2 style={{ fontSize: "1.4rem", float: "right", color: "grey" }}>
+        {props.resName}
+      </h2>
+      <h2 style={{ fontSize: "1rem" }}>
+        Order No.
+        <span style={{ color: "#4E944F", fontWeight: "700" }}> #{props.orderId}</span>
+      </h2>
+      <h2 style={{ fontSize: "1.2rem" }}>Items (2)</h2>
+      <h3 style={{ fontSize: "0.8rem", fontWeight: "400" , color:"grey"}}>Pizza , Burger</h3>
+      <hr />
+      <h2 style={{ fontSize: "1rem" }}>
+        Order placed on <span style={{ color: "#4E944F" }}>{props.date}</span>
+      </h2>
+      <h2 style={{ fontSize: "1rem" }}>
+        for Amount
+        <span style={{ color: "#4E944F", fontWeight: "700" }}> Rs. {props.total}</span>
+      </h2>
+    </div>
+  );
+}
+ 
--- src/pages/orderhistory.jsx
+++ src/pages/orderhistory.jsx
@@ -3,12 +3,35 @@
 // for now map it 3 times with static data
 import Navbar from "../components/navbar";
 import SimpleBottomNavigation from "../components/bottomnavigation";
-
+import Order from "../components/order";
 export default function OrderHistory() {
   return (
     <div>
       <Navbar />
-      <h1>This is Order History page</h1>
+      <div style={{ padding: "10px", marginTop: "10px" }}>
+        <h1 style={{ marginBottom: "10px", fontSize: "1.5rem" }}>All Orders</h1>
+        <Order
+          key="1"
+          resName="Restaurent X"
+          orderId="1000001"
+          date="07 May"
+          total="200"
+        />
+        <Order
+          key="2"
+          resName="Restaurent Y"
+          orderId="1000002"
+          date="07 May"
+          total="100"
+        />
+        <Order
+          key="3"
+          resName="Restaurent Z"
+          orderId="1000003"
+          date="07 May"
+          total="400"
+        />
+      </div>
       <SimpleBottomNavigation />
     </div>
   );
