17175445a22e588226ca7fe5ba7265171eaf8e02
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-05-07 14:51:12 +0530

    shadow added

--- src/components/order.jsx
+++ src/components/order.jsx
@@ -4,13 +4,11 @@ export default function Order(props) {
   
   return (
     <div
+      className="shadow p-3 mb-3 bg-body rounded"
       style={{
         padding: "10px",
         textAlign: "left",
         width: "100%",
-        backgroundColor: "#F9F3EE",
-        borderRadius: "10px",
-        marginBottom:"20px"
       }}
     >
       <h2 style={{ fontSize: "1.4rem", float: "right", color: "grey" }}>
@@ -18,17 +16,25 @@ export default function Order(props) {
       </h2>
       <h2 style={{ fontSize: "1rem" }}>
         Order No.
-        <span style={{ color: "#4E944F", fontWeight: "700" }}> #{props.orderId}</span>
+        <span style={{ color: "#4E944F", fontWeight: "700" }}>
+          {" "}
+          #{props.orderId}
+        </span>
       </h2>
       <h2 style={{ fontSize: "1.2rem" }}>Items (2)</h2>
-      <h3 style={{ fontSize: "0.8rem", fontWeight: "400" , color:"grey"}}>Pizza , Burger</h3>
+      <h3 style={{ fontSize: "0.8rem", fontWeight: "400", color: "grey" }}>
+        Pizza , Burger
+      </h3>
       <hr />
       <h2 style={{ fontSize: "1rem" }}>
         Order placed on <span style={{ color: "#4E944F" }}>{props.date}</span>
       </h2>
       <h2 style={{ fontSize: "1rem" }}>
         for Amount
-        <span style={{ color: "#4E944F", fontWeight: "700" }}> Rs. {props.total}</span>
+        <span style={{ color: "#4E944F", fontWeight: "700" }}>
+          {" "}
+          Rs. {props.total}
+        </span>
       </h2>
     </div>
   );
